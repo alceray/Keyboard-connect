d2f9937db78e75f14408d218e584c828a0912a83
alceray <39638378+alceray@users.noreply.github.com>
2024-02-26 01:42:52 -0500

    Add Sqlite

--- src/main.js
+++ src/main.js
@@ -4,12 +4,61 @@ import { join } from "path";
 import isDev from "electron-is-dev";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import sqlite3 from "sqlite3";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 let win = null;
 let tray = null;
+const dbPath = join(app.getPath("userData"), "database.db");
+
+let db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    return console.error("Error connecting to the database:", err.message);
+  }
+  console.log(`Connected to the database at ${dbPath}`);
+  db.exec(
+    `CREATE TABLE IF NOT EXISTS known_usb_devices (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      vid TEXT NOT NULL, 
+      pid TEXT NOT NULL,
+      name TEXT,
+      manufacturer TEXT,
+      is_connected BOOLEAN NOT NULL DEFAULT 0,
+      last_connected DATETIME,
+      last_disconnected DATETIME,
+      is_keyboard BOOLEAN NOT NULL DEFAULT 0,
+      UNIQUE(vid, pid)
+    );
+    CREATE TABLE IF NOT EXISTS connection_log (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      vid TEXT NOT NULL,
+      pid TEXT NOT NULL,
+      connected BOOLEAN NOT NULL,
+      timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(vid, pid) REFERENCES known_usb_devices(vid, pid)
+    );
+    CREATE TABLE IF NOT EXISTS all_keyboards (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      vid TEXT NOT NULL,
+      pid TEXT NOT NULL,
+      name TEXT NOT NULL,
+      json TEXT,
+      FOREIGN KEY(vid, pid) REFERENCES known_usb_devices(vid, pid)
+    );
+    CREATE INDEX IF NOT EXISTS idx_devices_vid_pid ON known_usb_devices(vid, pid);
+    CREATE INDEX IF NOT EXISTS idx_devices_is_connected ON known_usb_devices(is_connected);
+    CREATE INDEX IF NOT EXISTS idx_log_vid_pid ON connection_log(vid, pid);
+    CREATE INDEX IF NOT EXISTS idx_log_timestamp ON connection_log(timestamp);
+    CREATE INDEX IF NOT EXISTS idx_keyboards_vid_pid ON all_keyboards(vid, pid);`,
+    (err) => {
+      if (err) {
+        return console.error("Error creating the table:", err.message);
+      }
+    }
+  );
+});
 
 function safeSend(channel, data) {
   if (win) {
@@ -53,15 +102,14 @@ function createWindow() {
   if (isDev) {
     win.webContents.openDevTools();
   }
-  win.on('close', () => {
-    win = null
-  })
+  win.on("close", () => {
+    win = null;
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-});
+app.on("window-all-closed", () => {});
 
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
